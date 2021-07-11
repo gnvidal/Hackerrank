6e33ee4b4ba710a50860666a1ecd285a5d8dc6ff
gnvidal <45744107+gnvidal@users.noreply.github.com>
2021-07-10 20:44:29 -0400

    Update 6Write_a_function.py

--- CODE/Introduction/6Write_a_function.py
+++ CODE/Introduction/6Write_a_function.py
@@ -10,3 +10,30 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
+
+#Otro code 
+def is_leap(year):
+    leap = False
+    if (year%4==0) and (year%100!=0)or year%400=0):
+        leap=True
+
+year = int(input())
+print(is_leap(year))
+
+#Otro code 
+def is_leap(year):
+    leap = False
+    if year%4==0:
+        if year%100==0:
+            if year%400==0:
+                return True
+            else:
+                return False
+        else: 
+            return True
+    else:
+        return False
+        leap=True
+
+year = int(input())
+print(is_leap(year))
