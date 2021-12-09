a9e9ccf03b615586f88e8d3e2a39e6b7154dcc97
gnvidal <45744107+gnvidal@users.noreply.github.com>
2021-12-09 15:26:15 -0300

    Update 21 Designer Door Mat.py

--- CODE/Strings/21 Designer Door Mat.py
+++ CODE/Strings/21 Designer Door Mat.py
@@ -1,1 +1,14 @@
-#hola
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+c=input().split()
+n=int(c[0])
+m=int(c[1])
+
+    #top Arriba
+    #range(1,n,2) va de 1 a n y se salta la mitad porque tiene el 2
+for i in range(1,n,2):
+    print(('.|.'*i).center(m,'-'))
+    #middle al medio
+print('WELCOME'.center(m,'-'))
+    #bottom abajo
+for i in range(n-2,-1,-2): 
+    print(('.|.'*i).center(m,'-'))
